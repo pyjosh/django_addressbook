c1b6ece7e19939c1e333835634b77965248f8635
pyjosh <richard.richdude@gmail.com>
2013-09-16 22:21:23 +0200

    attempt to create separated form fro address

--- contacts/views.py
+++ contacts/views.py
@@ -10,8 +10,8 @@ import forms
 #The ListView that we subclass from is itself composed of several mixins that provide some behaviour !!!
 class ListContactView(ListView):
 
-	model = Contact                             # this view is going to list all the Contacts in our database
-	template_name = 'contact_list.html'
+    model = Contact                             # this view is going to list all the Contacts in our database
+    template_name = 'contact_list.html'
 
 
 
@@ -20,48 +20,48 @@ class ListContactView(ListView):
 # the user when the form is successfully submitted.
 class CreateContactView(CreateView):
 
-	model = Contact
-	template_name = 'edit_contact.html'
-	# after adding 'email_filed' in forms.py we have to tell this view to use it
-	form_class = forms.ContactForm
+    model = Contact
+    template_name = 'edit_contact.html'
+    # after adding 'email_filed' in forms.py we have to tell this view to use it
+    form_class = forms.ContactForm
 
-	def get_success_url(self):
-		return reverse('contacts-list')   # contact_list - the name of a view defined in URLS !
+    def get_success_url(self):
+        return reverse('contacts-list')   # contact_list - the name of a view defined in URLS !
 
-	# in order to use the same template for create and update (add/edit) we have to add info
-	# about where the form should redirect to the context
-	def get_context_data(self, **kwargs):
-		context = super(CreateContactView, self).get_context_data(**kwargs)
-		context['action'] = reverse('contacts-new')
+    # in order to use the same template for create and update (add/edit) we have to add info
+    # about where the form should redirect to the context
+    def get_context_data(self, **kwargs):
+        context = super(CreateContactView, self).get_context_data(**kwargs)
+        context['action'] = reverse('contacts-new')
 
-		return context
+        return context
 
 
 class UpdateContactView(UpdateView):
 
-	model = Contact
-	template_name = 'edit_contact.html'
-	# after adding 'email_filed' in forms.py we have to tell this view to use it
-	form_class = forms.ContactForm
+    model = Contact
+    template_name = 'edit_contact.html'
+    # after adding 'email_filed' in forms.py we have to tell this view to use it
+    form_class = forms.ContactForm
 
-	def get_success_url(self):
-		return reverse('contacts-list')
+    def get_success_url(self):
+        return reverse('contacts-list')
 
-	# in order to use the same template for create and update (add/edit) we have to add info
-	# about where the form should redirect to the context
-	def get_context_data(self, **kwargs):
-		context = super(UpdateContactView, self).get_context_data(**kwargs)
-		context['action'] = reverse('contacts-edit', kwargs = {'pk': self.get_object().id})
+    # in order to use the same template for create and update (add/edit) we have to add info
+    # about where the form should redirect to the context
+    def get_context_data(self, **kwargs):
+        context = super(UpdateContactView, self).get_context_data(**kwargs)
+        context['action'] = reverse('contacts-edit', kwargs = {'pk': self.get_object().id})
 
-		return context
+        return context
 
 class DeleteContactView(DeleteView):
 
-	model = Contact
-	template_name = 'delete_contact.html'
+    model = Contact
+    template_name = 'delete_contact.html'
 
-	def get_success_url(self):
-		return reverse('contacts-list')
+    def get_success_url(self):
+        return reverse('contacts-list')
 
 class ContactView(DetailView):
 
@@ -69,11 +69,11 @@ class ContactView(DetailView):
     template_name = 'contact.html'
 
     def get_context_data(self, **kwargs):
-		context = super(ContactView, self).get_context_data(**kwargs)
-		home_addresses = Address.objects.filter(contact_id=self.get_object().id, address_type='HOME')
-		context['home_address'] = home_addresses[0] if home_addresses else None
+        context = super(ContactView, self).get_context_data(**kwargs)
+        home_addresses = Address.objects.filter(contact_id=self.get_object().id, address_type='HOME')
+        context['home_address'] = home_addresses[0] if home_addresses else None
 
-		return context
+        return context
 
 
 
@@ -81,13 +81,28 @@ class ContactView(DetailView):
 #
 # to work with Contact and Address models together
 #
+# class EditContactAddressView(UpdateView):
+
+#     model = Contact                     # inline formset takes the parent object as its starting point
+#     template_name = 'edit_addresses.html'
+#     form_class = forms.ContactAddressFormSet
+
+#     def get_success_url(self):
+
+#         # redirect to the Contact view.
+#         return self.get_object().get_absolute_url()
+
 class EditContactAddressView(UpdateView):
 
-    model = Contact                     # inline formset takes the parent object as its starting point
+    model = Address                     # inline formset takes the parent object as its starting point
     template_name = 'edit_addresses.html'
-    form_class = forms.ContactAddressFormSet
+    #form_class = forms.ContactAddressFormSet
 
     def get_success_url(self):
+        return reverse('contacts-list')
 
-        # redirect to the Contact view.
-        return self.get_object().get_absolute_url()
+    def get_context_data(self, **kwargs):
+        context = super(EditContactAddressView, self).get_context_data(**kwargs)
+        context['action'] = reverse('contacts-edit-addresses', kwargs = {'pk': self.get_object().id})
+
+        return context
